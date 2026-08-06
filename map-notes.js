cdbb8072586ca2186304212337a171dff77510a4
geovanapereiracosta-jpg <geovana.pereira.costa@escola.pr.gov.br>
2026-08-06 14:41:42 +0000

    .

---
+++ map-notes.js
@@ -0,0 +1,7 @@
+const Nomes = ["ana Julia", "Caio vinícius", "BIA silva"];
+
+const NomesPadronizados = Nomes.map((Nome) => {
+    return Nome.toUpperCase();
+});
+
+console.log (NomesPadronizados);
--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 const notas = [10, 9.5, 8, 7, 6];
 
-notas.map(() => {
-
-
+const NotasAtualizadas = notas.map((nota) => {
+    return nota + 1 >= 10 ? 10 : nota + 1;
 });
+console.log(NotasAtualizadas);
